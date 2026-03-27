324d26955fe493e494efcd4aed65cc926e355017
Jahanwee . <jahanweeraj20@gmail.com>
2026-03-27 18:36:11 +0530

    T-class 26thMarch notes added

--- class_notes/t_class/12thFeb/script.js
+++
@@ -1,8 +0,0 @@
-// Javscript Refresher
-
-//hello world
-console.log("Hello people");
-
-//"use strict" must always stay on top 
-
-console.log(6-"3"); // 3 bcs other arithmetci operators work with num
